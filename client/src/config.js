1dcda11aab2a7da20c16677480ed861d1b13e350
Arya <aryachoudhary100@gmail.com>
2025-08-29 01:35:41 +0530

    updated the nase link

--- client/src/config.js
+++ client/src/config.js
@@ -1,1 +1,2 @@
-export const BACKEND_URL = 'http://127.0.0.1:5000';
+export const BACKEND_URL = 'https://influencer-jewq.onrender.com';
+// const BACKEND_URL = 'https://influencer-jewq.onrender.com';
